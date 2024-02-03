0be073b19aefa70a37af6db36f2f90838ad3ac98
KandyMun <rektelkid@gmail.com>
2024-02-03 23:29:16 +0200

    mod start
    yah

---
+++ src/main/java/name/gdlt/GDLTDataGenerator.java
@@ -0,0 +1,11 @@
+package name.gdlt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GDLTDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
